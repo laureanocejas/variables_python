3123422c1e5b31956484590d60d32792931d8429
laureanocejas <72823507+laureanocejas@users.noreply.github.com>
2020-10-20 17:33:26 -0300

    Add files via upload

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -17,32 +17,16 @@ __version__ = "1.4"
 
 def ej1():
     # Ejercicios de práctica numérica
+    
     numero_1 = 5
     numero_2 = 7
-
     suma = numero_1 + numero_2
     print ("El resultado de la suma es", suma)
     resta = numero_1 - numero_2
     print ("El resultado de restar", numero_1, "y", numero_2, "es", resta)
 
-    # Realizar la suma de las dos variables
-    # numero_1 y numero_2
-    # Almacenar el valor de la suma en una variable
-    # ej:
-    # operacion = .....
-
-    # Imprimir en pantalla el resultado de la suma
-    # print(....)
-
-    # Repita el procedimiento para realizar la resta
-
-
 def ej2():
-    # Ejercicios de práctica numérica
-
-    # Operadores con números decimales
-    # Ahora los valores a operar deben ser ingresados por
-    # consola con la función "input" como se ve a continuación
+    
     print('Ingrese el primer número decimal a operar:')
     numero_1 = float(input())
     print("El numero ingresado es", numero_1)
@@ -60,49 +44,16 @@ def ej2():
     print("el resultado de dividir", numero_1,"y", numero_2,"es", division)
  
 
-
-    # Alumno: Imprima en pantalla los dos números decimales solicitados
-    # print(....)
-
-    # Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
-    # numero_1, numero_2
-    # Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
-    # El resultado de sumar 4 y 2 es 6
-
-    # Suma
-
-    # Resta
-
-    # División
-
-    # Multiplicación
-
-
 def ej3():
-    # Ejemplos variables de texto
-
-    # Ingrese primero su nombre y luego su apellido
-    # Capture ambos datos e imprima su nombre completo
+    
     print('Ingrese su nombre/s:')
     nombre = str(input())
-
     print('Ingrese su apellido/s:')
     apellido = str(input())
-
     nombre_completo = nombre
     Nombre_len = len (nombre_completo)
     print (nombre+" "+apellido)
     print(nombre_completo,"tiene un total de", Nombre_len, "caracteres")
-    
-
-
-    # Imprima su nombre completo
-
-    # Almacenar su nombre completo en una variable
-    # nombre_completo = .....
-
-    # Imprimir la cantidad de letras que posee su nombre completo
-
 
 def ej4():
     # Ejemplos variables de texto
@@ -124,14 +75,11 @@ def ej4():
     print (caracter_inicial + caracter_inicial2 +  caracter_inicial3)
 
 
-    # De cada palabra debe tomar la primera letra y armar el acrónimo
-    # Ejemplo: Alumbrado, barrido y limpieza --> ABL
-    # Imprimir el resultado en pantalla
+    
 
 
 def ej5():
     # Ejemplos variables de texto
-
     # Ingrese dos palabras y arme combinaciones con ella
     print('Ingrese palabra 1:')
     palabra_1 = str(input())
